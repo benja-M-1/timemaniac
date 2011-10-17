ebd04854114583b968ad72b66a1f8f1abc9a54d4
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2011-10-18 00:37:17 +0200

    Add start, pause, stop method of Task

--- lib/timemaniac/model/task.rb
+++ lib/timemaniac/model/task.rb
@@ -1,9 +1,61 @@
 module Timemaniac
     module Model
         class Task
+            attr_accessor :id
             attr_accessor :name
-            def initialize(name)
-                @name = name
+            attr_accessor :description
+
+            attr_reader   :status
+            attr_reader   :started_at
+            attr_reader   :finished_at
+            attr_reader   :elapsed_time
+
+            def initialize(name, description = nil)
+                @name   = name
+                @description = description
+                @status = :unstarted
+                @elapsed_time = 0
+            end
+
+            # Starts the task.
+            # Set the status to started and set the started_at date.
+            # @return void
+            def start
+                if @status != :stopped or @status != :running
+                    @started_at = Time.now
+                    @status = :running
+                end
+            end
+
+            # Pauses the task.
+            # It calculates the elapsed time between the begining and the end.
+            # @return void
+            def pause
+                if @status == :running
+                    @finished_at = Time.now
+                    calculate_elapsed_time
+                    @status = :paused
+                end
+            end
+
+            # Stops the task.
+            # Set the status to stopped. he task can't be started again.
+            # @return void
+            def stop
+                unless @status == :stopped
+                    @finished_at = Time.now
+                    calculate_elapsed_time
+                    @status = :stopped
+                end
+            end
+
+            private
+
+            # Computes elapsed time between the last start date and finished date
+            # @return integer
+            def calculate_elapsed_time
+                raise Exception, "Can't calculate elapsed time" if @started_at.nil?
+                @elapsed_time += @finished_at.to_i - @started_at.to_i 
             end
         end
     end
--- spec/lib/timemaniac/model/task_spec.rb
+++ spec/lib/timemaniac/model/task_spec.rb
@@ -1,6 +1,38 @@
 describe Timemaniac::Model::Task do
-    it "test Task name method" do
+    it "test Task's name method" do
         task = Timemaniac::Model::Task.new "test"
         task.name.should == "test"
+    end
+
+    it "test Task's description method" do
+        task = Timemaniac::Model::Task.new "test"
+        task.description = "Lorem ipsum dolor blablabla..."
+        task.description.should == "Lorem ipsum dolor blablabla..."
+    end
+
+    it "test Task's start method" do
+        task = Timemaniac::Model::Task.new "test" "description"
+        task.status.should == :unstarted
+        task.started_at.should be_nil
+
+        task.start
+        task.status.should == :running
+        task.started_at.should be_an_instance_of Time
+    end
+
+    it "test Task's pause method" do
+        task = Timemaniac::Model::Task.new "test" "description"
+        task.start
+        task.pause
+        task.status.should == :paused
+        task.finished_at.should be_an_instance_of Time
+    end
+
+    it "test Task's stop method" do
+        task = Timemaniac::Model::Task.new "test" "description"
+        task.start
+        task.stop
+        task.status.should == :stopped
+        task.finished_at.should be_an_instance_of Time
     end
 end
